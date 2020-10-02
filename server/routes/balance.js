363b169713c88d7d06e43870f5d12abb8b02eba8
Hannes Juurma <hannes@eagronom.com>
2020-10-02 12:19:48 +0300

    As an user, I want to be able to check my balance. [Delivers #174595703] Squashed commits: [3665287] Create a balance route and reference it at index.js. Enable authentication validation and find logged in user's balance.

---
+++ server/routes/balance.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const verifyUser = require('./authorizations/authUser');
+
+const Account = require("../models/Account");
+
+// Add verification middleware.
+router.get('/', verifyUser, async (req, res) => {
+  try {
+    // Find the account of the logged in user (id stored in req.user).
+    const balanceAccount = await Account.findOne({ 'user': req.user._id }).select('balance');
+
+    res.status(200).json({"Your balance": balanceAccount.balance + " euro"});
+  } catch(err) {
+    res.status(401).json( {"error": "Unauthorized action. Log in to do that." });
+  }
+})
+
+module.exports = router;
