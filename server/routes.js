025af4ce013fa955295d84ffc9efbd185b1d46e7
Hannes Juurma <hannes@eagronom.com>
2020-10-05 11:06:36 +0300

    As an user, I want to be able to withdraw cash. [Delivers #174595838] Squashed commits: [aecd7fe] Create a route for withdrawing. Let account who is logged in to withdraw money.

--- server/routes.js
+++ server/routes.js
@@ -4,3 +4,4 @@ module.exports.usersRouter = require("./routes/users");
 module.exports.sessionsRouter = require('./routes/sessions');
 module.exports.balanceRouter = require("./routes/balance");
 module.exports.depositRouter = require("./routes/deposit");
+module.exports.withdrawRouter = require("./routes/withdraw");
